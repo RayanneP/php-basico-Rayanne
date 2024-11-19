feb1080d90ba289851246af5d0a8d3a1f3b77b62
RayanneP <rayannepicinidasilva@gmail.com>
2024-11-19 11:02:37 -0300

    Update 9_conexao_bd.php

--- 9_conexao_bd.php
+++ 9_conexao_bd.php
@@ -4,16 +4,34 @@
 
 <!-- Para criar a Tabela -->
 <!-- 
-    CREATE TABLE clientes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
-    ); 
+    
 -->
 
 <?php
 
-// Digitar PHP
+//ALTERAR de acordo com o número da porta
+//verificar conexão com XAMPP
+$servername = "localhost:3306";
+$username = "root";
+$password ="";
+$dbname = "exercicio";
+
+try{
+    //Tenta criar uma conexão com o banco de dados
+    //Confere se as informações estão corretas
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    //Verifica se houve algum erro na conexão
+    if ($conn->connect_error){
+        throw new Exception ("Falha na conexão: " . $conn->connect_error);
+    }
+
+    echo "Conexão realizada com sucesso!";
+} catch (Exception $e){
+    //Exibe uma mensagem de erro amigável
+    echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
+
+}
 
 ?>
 
